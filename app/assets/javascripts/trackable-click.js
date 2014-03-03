8f40f37c1ea400143da282d2ad88d44bf4e70bbb
Christopher Parratto <cparratto@gmail.com>
2014-03-02 16:17:17 -0800

    Added support for ga ecommerce tracking and conditional tracking.

--- app/assets/javascripts/trackable-click.js
+++ app/assets/javascripts/trackable-click.js
@@ -1,29 +1,77 @@
 $(document).ready(function() {
+    function trackableClickOnlyIf(element, prefix) {
+      var trackable = ($(element).data(prefix + '-only-if') &&
+          eval($(element).data(prefix + '-only-if'))) ||
+          !$(element).data(prefix + '-only-if');
+      return trackable;
+    }
+
+    function trackableClickEvalAttr(element, dataAttr) {
+        var attr = '';
+
+        try {
+            attr = eval($(element).data(dataAttr));
+        } catch (e) {
+            attr = $(element).data(dataAttr);
+        }
+
+        return attr;
+    }
+
     $('body').delegate( ".trackable-click", "click", function() {
         if($(this).data('tc-fb-pixel-id') &&
            $(this).data('tc-fb-pixel-value') &&
            $(this).data('tc-fb-pixel-currency')) {
+
+            if(trackableClickOnlyIf(this, 'tc-fb-pixel')) {
                 window.fb_param = {}; // must be global by adding `window.`
                 fb_param.pixel_id = $(this).data('tc-fb-pixel-id');
-                fb_param.value = $(this).data('tc-fb-pixel-value');
-                fb_param.currency = $(this).data('tc-fb-pixel-currency');
+                fb_param.value = trackableClickEvalAttr(this, 'tc-fb-pixel-value');
+                fb_param.currency = trackableClickEvalAttr(this,'tc-fb-pixel-currency');
 
                 (function(){
                     var fpw = document.createElement('script'); fpw.async = true; fpw.src = '//connect.facebook.net/en_US/fp.js';
                     var ref = document.getElementsByTagName('script')[0];
                     ref.parentNode.insertBefore(fpw, ref);
                 })();
+            }
         }
 
         if($(this).data('tc-google-event-action') &&
            $(this).data('tc-google-event-label')){
-            var action = $(this).data('tc-google-event-action');
-            var label = $(this).data('tc-google-event-label');
+            var action = trackableClickEvalAttr(this,'tc-google-event-action');
+            var label = trackableClickEvalAttr(this,'tc-google-event-label');
 
-            if($(this).data('tc-google-button-event-value')) {
-                ga('send', 'event', 'button', action , label, $(this).data('tc-google-button-event-value'));
-            } else {
-                ga('send', 'event', 'button', action , label);
+            if(trackableClickOnlyIf(this, 'tc-google-event')) {
+                if($(this).data('tc-google-button-event-value')) {
+                    ga('send', 'event', 'button', action , label, $(this).data('tc-google-button-event-value'));
+                } else {
+                    ga('send', 'event', 'button', action , label);
+                }
+            }
+        }
+
+        if($(this).data('tc-google-commerce-id') &&
+           $(this).data('tc-google-commerce-affiliation') &&
+           $(this).data('tc-google-commerce-revenue') &&
+           $(this).data('tc-google-commerce-shipping') &&
+           $(this).data('tc-google-commerce-tax')){
+
+            var id = trackableClickEvalAttr(this,'tc-google-commerce-id');
+            var affiliation = trackableClickEvalAttr(this,'tc-google-commerce-affiliation');
+            var revenue = trackableClickEvalAttr(this,'tc-google-commerce-revenue');
+            var shipping = trackableClickEvalAttr(this,'tc-google-commerce-shipping');
+            var tax = trackableClickEvalAttr(this,'tc-google-commerce-tax');
+
+            if(trackableClickOnlyIf(this, 'tc-google-commerce')) {
+                ga('ecommerce:addTransaction', {
+                    'id': id,
+                    'affiliation': affiliation,
+                    'revenue': revenue,
+                    'shipping': shipping,
+                    'tax': tax
+                });
+                ga('ecommerce:send');
             }
         }
     });
--- lib/trackable-click/view_helpers.rb
+++ lib/trackable-click/view_helpers.rb
@@ -26,10 +26,12 @@ module TrackableClick
       options[:class] = make_trackable_class_from_options(options)
       fp_pixel_data_attrs = make_fp_pixel_data_attrs_from_options!(options)
       google_event_attrs = make_google_event_data_attrs_from_options!(options)
+      google_commerce_attrs = make_google_commerce_data_attrs_from_options!(options)
 
       options[:data] ||= {}
       options[:data].merge!(fp_pixel_data_attrs)
       options[:data].merge!(google_event_attrs)
+      options[:data].merge!(google_commerce_attrs)
     end
 
     def make_trackable_class_from_options(options)
@@ -43,6 +45,7 @@ module TrackableClick
       fp_pixel_options = options[:fp_pixel]
 
       unless fp_pixel_options.nil?
+         data_attrs[:tc_fb_pixel_only_if] = fp_pixel_options[:only_if]
          data_attrs[:tc_fb_pixel_id] = fp_pixel_options[:id]
          data_attrs[:tc_fb_pixel_value] = fp_pixel_options[:value]
          data_attrs[:tc_fb_pixel_currency] = fp_pixel_options[:currency]
@@ -57,6 +60,7 @@ module TrackableClick
        google_event_options = options[:google_event]
 
        unless google_event_options.nil?
+         data_attrs[:tc_google_event_only_if] = google_event_options[:only_if]
          data_attrs[:tc_google_event_action] = google_event_options[:action]
          data_attrs[:tc_google_event_label] = google_event_options[:label]
 
@@ -67,6 +71,23 @@ module TrackableClick
 
        options.delete(:google_event)
        data_attrs
+    end
+
+    def make_google_commerce_data_attrs_from_options!(options)
+      data_attrs = {}
+      google_commerce_options = options[:google_commerce]
+
+      unless google_commerce_options.nil?
+        data_attrs[:tc_google_commerce_only_if] = google_commerce_options[:only_if]
+        data_attrs[:tc_google_commerce_id] = google_commerce_options[:id]
+        data_attrs[:tc_google_commerce_affiliation] =google_commerce_options[:affiliation]
+        data_attrs[:tc_google_commerce_revenue] = google_commerce_options[:revenue]
+        data_attrs[:tc_google_commerce_shipping] = google_commerce_options[:shipping]
+        data_attrs[:tc_google_commerce_tax] = google_commerce_options[:tax]
+      end
+
+      options.delete(:google_commerce)
+      data_attrs
     end
   end
 end
